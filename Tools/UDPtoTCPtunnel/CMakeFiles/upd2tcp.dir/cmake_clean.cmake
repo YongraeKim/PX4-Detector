97bbc2f77054ee56e5659dfe9bdc29f90e58c7d4
YongraeKim <kimyr911125@gmail.com>
2021-09-20 13:42:27 +0900

    First commit of UDP2TCP tunneling. Success on opening UDP socket : port 14570

---
+++ Tools/UDPtoTCPtunnel/CMakeFiles/upd2tcp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/upd2tcp.dir/src/main.cpp.o"
+  "upd2tcp.pdb"
+  "upd2tcp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/upd2tcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
