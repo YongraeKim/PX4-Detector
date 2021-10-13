2389076feec7a28eacbcf84cbf6784cd64d7f240
YongraeKim <kimyr911125@gmail.com>
2021-10-13 21:42:20 +0900

    bug fix for setup udp2tcp

--- Tools/UDPtoTCPtunnel/setup/setup_udp2tcp.sh
+++ Tools/UDPtoTCPtunnel/setup/setup_udp2tcp.sh
@@ -7,7 +7,7 @@ printf "move udp2tcp to /usr/local/bin\n"
 sudo cp udp2tcp /usr/local/bin
 cd setup
 sudo chmod 755 udp2tcp_service.service
-sudo cp udp2tcp_service /etc/systemd/system/
+sudo cp udp2tcp_service.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable udp2tcp_service.service
 systemctl start udp2tcp_service.service
